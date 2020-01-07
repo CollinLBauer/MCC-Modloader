fd96e28ac445e671845a2d9b7c2d03b7d4c6ed3b
CommieSlayer1950 <nathan.leakeas@gmail.com>
2020-01-07 16:25:19 -0600

    Updated actions file with basic functions for the modloader

--- actions.py
+++ actions.py
@@ -5,7 +5,7 @@ import shutil
 
 #Utilities file, used by gui to perform correct actions
 
-def vanilla():
+def vanilla(dir):
   for subdir, dirs, files in os.walk(dir):
     for filename in files:
         file = subdir + os.sep + filename
@@ -13,21 +13,23 @@ def vanilla():
         if ext == ".original":
             shutil.move(os.path.join(dir, file),os.path.join(dir, base))
             shutil.copyfile(os.path.join(dir, base), os.path.join(dir, base+'.original'))
-  return False
   print("Halo MCC Ready to play without mods")
+  return False
 
-  def modded():
-    for subdir, dirs, files in os.walk(dir):
-      for filename in files:
-        file = subdir + os.sep + filename
-        base, ext = os.path.splitext(file)
-        if ext == ".mod":
-          shutil.move(os.path.join(dir, file),os.path.join(dir, base))
-          shutil.copyfile(os.path.join(dir, base), os.path.join(dir, base+'.mod'))
-      return True
+
+def modded(dir):
+  for subdir, dirs, files in os.walk(dir):
+    for filename in files:
+      file = subdir + os.sep + filename
+      base, ext = os.path.splitext(file)
+      if ext == ".mod":
+        shutil.move(os.path.join(dir, file),os.path.join(dir, base))
+        shutil.copyfile(os.path.join(dir, base), os.path.join(dir, base+'.mod'))
   print("Halo MCC Ready to play with mods")
+  return True
 
-def modlist():
+
+def modlist(dir):
   #print("Mods:")
   modl=[]
   for subdir, dirs, files in os.walk(dir):
