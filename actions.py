f6a490049717185160b8a111471bdae52f4d3f68
ib4519894 <37911433+ib4519894@users.noreply.github.com>
2020-01-09 16:13:23 -0600

    Update actions.py

--- actions.py
+++ actions.py
@@ -1,9 +1,10 @@
 import os
 import sys
 from pathlib import Path
-import shutil 
+import shutil
+import hashlib
 
-#Utilities file, used by gui to perform correct actions
+#Utilities file, used by gui and cmd to perform correct actions
 
 def vanilla(dir):
   for subdir, dirs, files in os.walk(dir):
@@ -39,6 +40,41 @@ def modlist(dir):
         if ext == ".mod":
             modl.append(filename)
   return modl
+
+
+
+BLOCKSIZE = 65536
+# Compare SHA256 of current file with original file
+# Return true if the same (vanilla)
+# Return false if different (modded)
+def check_if_modded(filename):
+  
+  # hash current file
+  hasher = hashlib.sha256()
+  with open(filename, "rb") as current:
+    buf = current.read(BLOCKSIZE)
+    while len(buf) > 0:
+      hasher.update(buf)
+      buf = current.read(BLOCKSIZE)
+  curr_hash = hasher.hexdigest()
+
+  # hash original file
+  hasher = hashlib.sha256()
+  with open(filename + ".original", "rb") as original:
+    buf = original.read(BLOCKSIZE)
+    while len(buf) > 0:
+      hasher.update(buf)
+      buf = original.read(BLOCKSIZE)
+  orig_hash = hasher.hexdigest()
+  
+  #print(curr_hash) #debug
+  #print(orig_hash) #debug
+
+  # Compare hashes and return result
+  for i in range(len(curr_hash)):
+    if curr_hash[i] != orig_hash[i]:      
+      return False # Files are different
+  return True # Files are the same
 
 #def launch(modded,steam):
 #    if(shouldlaunch.get()):
