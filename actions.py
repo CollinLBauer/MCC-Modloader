08c1012f62599874904e74ef59b70f67825dcc57
ib4519894 <37911433+ib4519894@users.noreply.github.com>
2020-01-09 16:18:57 -0600

    Update actions.py

--- actions.py
+++ actions.py
@@ -6,43 +6,6 @@ import hashlib
 
 #Utilities file, used by gui and cmd to perform correct actions
 
-def vanilla(dir):
-  for subdir, dirs, files in os.walk(dir):
-    for filename in files:
-        file = subdir + os.sep + filename
-        base, ext = os.path.splitext(file)
-        if ext == ".original":
-            shutil.move(os.path.join(dir, file),os.path.join(dir, base))
-            shutil.copyfile(os.path.join(dir, base), os.path.join(dir, base+'.original'))
-  print("Halo MCC Ready to play without mods")
-  return False
-
-
-def modded(dir):
-  for subdir, dirs, files in os.walk(dir):
-    for filename in files:
-      file = subdir + os.sep + filename
-      base, ext = os.path.splitext(file)
-      if ext == ".mod":
-        shutil.move(os.path.join(dir, file),os.path.join(dir, base))
-        shutil.copyfile(os.path.join(dir, base), os.path.join(dir, base+'.mod'))
-  print("Halo MCC Ready to play with mods")
-  return True
-
-
-def modlist(dir):
-  #print("Mods:")
-  modl=[]
-  for subdir, dirs, files in os.walk(dir):
-    for filename in files:
-        file = subdir + os.sep + filename
-        base, ext = os.path.splitext(file)
-        if ext == ".mod":
-            modl.append(filename)
-  return modl
-
-
-
 BLOCKSIZE = 65536
 # Compare SHA256 of current file with original file
 # Return true if the same (vanilla)
