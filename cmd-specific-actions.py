08b0ceeec0f90628f72266654773f27b62ebdf80
ib4519894 <37911433+ib4519894@users.noreply.github.com>
2020-01-09 16:10:18 -0600

    Update cmd-specific-actions.py

--- cmd-specific-actions.py
+++ cmd-specific-actions.py
@@ -1,1 +1,24 @@
+import os
+import sys
+from pathlib import Path
+import shutil 
 
+def backup_file(filename):
+  print("Not yet implemented.")
+  pass
+
+def backup_original():
+  print("Not yet implemented.")
+  pass
+
+'''
+print("""This program will backup all vanilla map files.
+WARNING: This utility assumes all currently active .map files are vanilla.
+Any currently backed up map files will be lost or replaced.
+Are you sure you want to continue? y/n: """,end="")
+  answer = ""
+  while answer != "Y" and answer != "N":
+    answer = input().upper()
+  if answer == "Y":
+    backup_original()
+'''
