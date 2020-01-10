8363788e4a54c7014ed5cf70ddaf4853130a5825
Collin <collinlbauer@gmail.com>
2020-01-10 01:34:35 -0500

    backup_file

--- backup_vanilla.py
+++ backup_vanilla.py
@@ -1,37 +1,53 @@
 import os
-import sys
+import shutil
 import json
 
-with open("settings.json", "r") as settings:
-  dir = json.loads(settings.read())["directory"]
 
-
-def backup_file(filename):
+def backup_file(filepath, force=False):
+  # uses copyfile to back up file with the .original tag amended
+  # tbh I prefer a folder attempt to tags, but the project's brand new so whatever.
+  backfile = filepath + ".original"
+  if not force and os.path.exists(backfile):
+    return
   
-  pass
+  shutil.copyfile(filepath, backfile)
 
-def backup_original():
-  print("Not yet implemented.")
+
+def backup_originals(directory, gui=False):
+  #TODO
+  '''
+  print("Backing up files...", end="")
+  for each file in directory:
+    if file ends in ".map":
+      send it to backup_file()
+      if gui=False:
+        print(".", end="", dumpbuffer)
+  print("Done.")
+  '''
+
+  print("Not yet implemented!")
   pass
 
 
 def main():
+  with open("settings.json", "r") as settings:
+    dir = json.loads(settings.read())["directory"]
+
   if not os.path.exists(dir):
     print("Map directory not found!\nPlease change the directoy in settings.json")
     return
   
   print("""This program will backup all vanilla map files.
 WARNING: This utility assumes all currently active .map files are vanilla.
-Any currently backed up map files will be lost.
 
-Are you sure you want to continue? y/n: """,
+Continue? y/n: """,
   end="")
 
   answer = ""
   while answer != "Y" and answer != "N":
     answer = input().upper()
   if answer == "Y":
-    backup_original()
+    backup_originals(dir)
 
 
 main()
