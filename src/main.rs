b2b6b9a0a95aa37098b52e89a3e2bf7a272c08a8
Collin <collinlbauer@gmail.com>
2020-04-22 19:42:55 -0400

    tiny progress

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,43 @@ pub mod tools;
 use std::env;
 use tools::mod_status::check_if_modded;
 
-fn help() {
-    println!("I don't do anything yet!");
+fn cli() {
+    println!("Eventually CLI functionality will go here.");
+    println!("CLI is mostly for testing and early implementation.");
+}
+
+fn gui() {
+    println!("Eventually GUI functionality will go here.");
+}
+
+fn help_message() {
+    println!("Invalid usage.");
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    for arg in args {
-        println!("{}",arg );
-    
+    match args.len() {
+        1 => {
+            gui();
+        }
+        2 => {
+            match args[1].as_str() {
+                "-cli" => {
+                    cli();
+                }
+                "-gui" => {
+                    gui();
+                }
+                _ => {
+                    help_message();
+                }
+            }
+        }
+        _ => {
+            help_message();
+        }
     }
-    help();
-    tools::mod_status::check_if_modded("hi".to_string(), "hello".to_string());
+
+    println!("Done.");
 }
--- src/tools/mod_status.rs
+++ src/tools/mod_status.rs
@@ -1,5 +1,10 @@
 use md5;
+use std::fs;
 
-pub fn check_if_modded(filename:String, title:String) {
-    println!("I don't work yet!");
+pub fn check_if_modded(file_path:String, title:String) {
+    println!("Under construction.");
+    // hash the current file
+    //let mut file = fs::File::open(&file_path)?;
+    //let hash = md5::digest_
+
 }
